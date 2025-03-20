ac3f47b172d92aa9ea6296fc24e7555318c9baee
goodlckkk <smaringonz@gmail.com>
2025-03-20 13:02:40 -0300

    wip final

--- my-game-page/src/components/main.tsx
+++ my-game-page/src/components/main.tsx
@@ -10,7 +10,6 @@ import LoadingSpinner from "../loading/loading";
 import { initFlowbite } from "flowbite";
 
 const mainPage = () => {
-  // Estados para almacenar datos y filtros
   const [games, setGames] = useState([]);
   const [genres, setGenres] = useState([]);
   const [platforms, setPlatforms] = useState([]);
@@ -21,27 +20,19 @@ const mainPage = () => {
   const [selectedPlatform, setSelectedPlatform] = useState("");
   const [selectedTags, setSelectedTags] = useState("");
   const [selectedPublisher, setSelectedPublisher] = useState("");
-  const [searchTerm, setSearchTerm] = useState(""); // Nuevo estado para el término de búsqueda
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true);
 
-  // useEffect para inicializar Flowbite y cargar juegos y filtros al montar el componente
   useEffect(() => {
     initFlowbite();
+
     fetchGames();
     fetchFilters();
-  }, [
-    currentPage,
-    selectedGenre,
-    selectedYear,
-    selectedPlatform,
-    selectedTags,
-    selectedPublisher,
-    searchTerm,
-  ]); // Añadir dependencias
+  }, [currentPage]);
 
-  // Función para obtener juegos aplicando los filtros
   const fetchGames = () => {
+    setLoading(true);
     const filters: any = {
       page: currentPage,
       genres: selectedGenre,
@@ -49,60 +40,61 @@ const mainPage = () => {
       platforms: selectedPlatform,
       tags: selectedTags,
       publishers: selectedPublisher,
-      search: searchTerm, // Incluir el término de búsqueda
     };
 
     getGames(filters).then((data) => {
       setGames(data.results);
       console.log(data);
       setTotalPages(data.count ? Math.ceil(data.count / 10) : 1); // Ajusta según el tamaño de página
+      setLoading(false);
     });
   };
 
-  // Función para obtener los datos de los filtros
   const fetchFilters = () => {
+    setLoading(true);
     Promise.all([getGenres(), getPlataform(), getTags(), getPublishers()]).then(
       ([genresData, platformsData, tagsData, publishersData]) => {
         setGenres(genresData.results);
         setPlatforms(platformsData.results);
         setTags(tagsData.results);
         setPublisher(publishersData.results);
+        setLoading(false);
       }
     );
   };
 
-  // Función para aplicar los filtros y reiniciar la página a la primera
   const applyFilters = () => {
     setCurrentPage(1);
     fetchGames();
   };
 
-  // Función para limpiar los filtros y reiniciar la página a la primera
   const clearFilters = () => {
     setSelectedGenre("");
     setSelectedYear("");
     setSelectedPlatform("");
     setSelectedTags("");
     setSelectedPublisher("");
-    setSearchTerm(""); // Limpiar el término de búsqueda
     setCurrentPage(1);
     fetchGames();
   };
 
-  // Función para manejar la búsqueda por nombre
   const searchByName = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setSearchTerm(value); // Actualizar el estado del término de búsqueda
+
+    getGames({ search: value }).then((data) => {
+      setGames(data.results);
+    });
   };
 
-  // Funciones para manejar los cambios en los filtros
   const filterByGenre = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const value = e.target.value;
+
     setSelectedGenre(value);
   };
 
   const filterByDate = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const year = e.target.value;
+
     setSelectedYear(year); // Actualiza el estado del año seleccionado
   };
 
@@ -110,40 +102,47 @@ const mainPage = () => {
 
   const filterByPlatforms = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const platform = e.target.value;
+
     setSelectedPlatform(platform);
   };
 
   const filterByTags = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const tag = e.target.value;
+
     setSelectedTags(tag);
   };
 
   const filterByPublishers = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const publisher = e.target.value;
+
     setSelectedPublisher(publisher);
   };
 
-  // Función para manejar el cambio de página
   const handlePageChange = (page: number) => {
     if (page > 0 && page <= totalPages) {
       setCurrentPage(page);
     }
   };
 
-  // Función para obtener el rango de paginación
   const getPaginationRange = () => {
     const start = Math.max(1, currentPage - 5);
     const end = Math.min(totalPages, currentPage + 5);
     return Array.from({ length: end - start + 1 }, (_, i) => start + i);
   };
 
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <>
-      <section className="bg-blend-luminosit bg-gray-900">
+      <section className="bg-blend-luminosity bg-gray-900">
         <div className="px-4 mx-auto max-w-screen-xl text-center">
           <div className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform translate-x-0">
-            <div className="h-full px-3 py-4 overflow-y-auto bg-gray-800">
-              <h3 className="mb-4 text-xl font-bold text-white">Filtros</h3>
+            <div className="h-full px-3 py-4 overflow-y-auto bg-slate-900 dark:bg-gray-800">
+              <h3 className="mb-4 text-xl font-bold text-white dark:text-white">
+                Filtros
+              </h3>
               <div className="space-y-4">
                 {/* Filtro de géneros */}
                 <select
@@ -268,7 +267,7 @@ const mainPage = () => {
           {/* Contenedor de la cuadricula */}
           <div className="grid py-4 gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {games.map((item: any, index) => (
-              <div key={index} className="text-gray-500">
+              <div key={index} className=" text-gray-500">
                 <a href={`/${item.id}`}>
                   <img
                     className="mx-auto mb-4 w-50 h-50 rounded-lg"
@@ -278,16 +277,16 @@ const mainPage = () => {
                   <h5 className="mb-1 text-2xl font-bold tracking-tight text-white">
                     {item.name}
                   </h5>
-                  <span className="text-start">
-                    {" "}
-                    {item?.platforms
-                      ?.map((platform: any) => platform.platform.name)
-                      .join(", ")}
-                  </span>
-                  <p>Puntuación: {item.rating}</p>
-                  <p>Metacritic: {item.metacritic}</p>
-                  <p>Fecha de lanzamiento: {item.released}</p>
                 </a>
+                <span>
+                  {" "}
+                  {item?.platforms
+                    ?.map((platform: any) => platform.platform.name)
+                    .join(", ")}
+                </span>
+                <p>Puntuación: {item.rating}</p>
+                <p>Metacritic: {item.metacritic}</p>
+                <p>Fecha de lanzamiento: {item.released}</p>
               </div>
             ))}
           </div>
@@ -298,7 +297,7 @@ const mainPage = () => {
               <li>
                 <a
                   href="#"
-                  className="flex items-center justify-center px-3 h-8 ms-0 leading-tight border border-e-0 rounded-s-lg  bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white"
+                  className="flex items-center justify-center px-3 h-8 leading-tight  rounded-s-lg bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white"
                   onClick={() => handlePageChange(currentPage - 1)}
                 >
                   Previous
@@ -322,7 +321,7 @@ const mainPage = () => {
               <li>
                 <button
                   type="button"
-                  className="flex items-center justify-center px-3 h-8 leading-tight  rounded-e-lg  bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white"
+                  className="flex items-center justify-center px-3 h-8 leading-tight  rounded-e-lg bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white"
                   onClick={() => handlePageChange(currentPage + 1)}
                 >
                   Next
